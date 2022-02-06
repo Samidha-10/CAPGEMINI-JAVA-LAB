d5bfb1476901ec2f35e7eac1d7c6392409fbad83
Samidha <98950177+Samidha-10@users.noreply.github.com>
2022-02-06 22:11:54 +0530

    Create ABC.java

---
+++ ABC.java
@@ -0,0 +1,16 @@
+package com.xyz;
+
+public class ABC {
+		  public static void main(String[] args) {
+		    int number = 10;
+		    if (number > 0) {
+		      System.out.println("The number is positive.");
+		    }
+		    
+		    else {
+		      System.out.println("The number is not positive.");
+		    }
+
+		    System.out.println("Statement outside if...else block");
+		  }
+		}
