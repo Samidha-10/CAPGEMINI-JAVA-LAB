d17f7945f6f7945d537f1032a1f1ccc417b92907
Samidha <98950177+Samidha-10@users.noreply.github.com>
2022-02-06 21:54:23 +0530

    Create Example2.java

---
+++ Example2.java
@@ -0,0 +1,18 @@
+package com.xyz;
+
+public class Example2 {
+
+	
+	public static void main(String args[]) {
+		
+		int x = 1;
+		{
+			int z;
+			int y =2;
+		}
+		int y =1;
+		int z = 3;
+		System.out.println(z++);
+		System.out.println(++z);
+	}
+}
