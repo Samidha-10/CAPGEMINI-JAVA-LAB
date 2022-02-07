8674dad7dbdc8b947a29c4840a07e1cabaa6752b
Samidha <98950177+Samidha-10@users.noreply.github.com>
2022-02-07 11:11:13 +0530

    Create trimCheck.java

---
+++ trimCheck.java
@@ -0,0 +1,10 @@
+package com.xyz;
+
+public class trimCheck {
+	  public static void main(String[] args) {
+		    String str1 = "     Learn   Java Programming ";
+		    String str2 = "Learn\nJava Programming\n\n   ";
+		    System.out.println(str1.trim());
+		    System.out.println(str2.trim());
+		  }
+}
