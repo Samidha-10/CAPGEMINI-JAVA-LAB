2f9e9237c56e6d0a96192f0f6cdc78248518f805
Samidha <98950177+Samidha-10@users.noreply.github.com>
2022-02-07 10:35:32 +0530

    Create displaynum.java

---
+++ displaynum.java
@@ -0,0 +1,12 @@
+package com.xyz;
+import java.util.Scanner;
+public class displaynum {
+	  public static void main(String[] args) {
+
+	    int i = 1, n = 5;
+	    do {
+	      System.out.println(i);
+	      i++;
+	    } while(i <= n);
+	  }
+	}
