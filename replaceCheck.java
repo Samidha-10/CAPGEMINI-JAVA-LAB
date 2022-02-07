b74e6282854e852bd8a766c6474e7099a2d9393e
Samidha <98950177+Samidha-10@users.noreply.github.com>
2022-02-07 11:03:42 +0530

    Create replaceCheck.java

---
+++ replaceCheck.java
@@ -0,0 +1,11 @@
+package com.xyz;
+
+public class replaceCheck {
+	  public static void main(String[] args) {
+		    String str1 = "C++ Programming";
+		    System.out.println(str1.replace("C++", "Java"));
+		    System.out.println("aa bb aa zz".replace("aa", "zz"));
+		    System.out.println("Java".replace("C++", "C"));
+		  }
+
+}
